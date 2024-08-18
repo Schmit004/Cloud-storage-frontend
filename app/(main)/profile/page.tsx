c549236e43cf605d83bb36960d098cff4af4d070
Schmitt <twitschmit@gmail.com>
2024-08-18 22:35:02 +0300

    Добавляет страницу с профилем пользователя

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,7 +1,50 @@
-export default function Profile() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { NextPage } from "next";
+import { Spin } from "antd";
+import { User } from "@/api/dto/auth.dto";
+import styles from "@/styles/Profile.module.scss";
+import * as Api from "@/api";
+
+const ProfilePage: NextPage = () => {
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const data = await Api.auth.getMe();
+      setUserData(data);
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!userData) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <Spin size="large" />
+      </div>
+    )
+  }
+
   return (
     <main>
-      <h1>Profile</h1>
+      <div className={styles.root}>
+        <h1>Мой профиль</h1>
+        <br />
+        <p>
+          ID: <b>{userData.id}</b>
+        </p>
+        <p>
+          Полное имя: <b>{userData.fullName}</b>
+        </p>
+        <p>
+          E-Mail: <b>{userData.email}</b>
+        </p>
+        <br />
+      </div>
     </main>
   );
-}
+};
+
+export default ProfilePage;
